3266ad9e13e75ae3ba277b5ac5239397e725b073
Ruben Bohorquez <r.bohorquezg3177@gmail.com>
2025-04-15 17:45:26 -0300

    Added utils and fixed mains

--- filters/filter_spain_2000/main.go
+++ filters/filter_spain_2000/main.go
@@ -1,21 +1,33 @@
 package filter_spain_2000
 
 import (
+	"distribuidos-tp1/common/utils"
 	"distribuidos-tp1/common/worker"
 )
 
 func main() {
+	v, err := utils.InitConfig()
+	if err != nil {
+		log.Criticalf("%s", err)
+	}
+
+	if err := utils.InitLogger(v.GetString("log.level")); err != nil {
+		log.Criticalf("%s", err)
+	}
+
+	utils.PrintConfig(v)
+
 	filter := NewFilterBySpainAndOf2000(FilterBySpainAndOf2000Config{
 		WorkerConfig: worker.WorkerConfig{
-			InputExchange:  "input_exchange",
-			OutputExchange: "output_exchange",
-			MessageBroker:  "amqp://guest:guest@localhost:5672/",
+			InputExchange:  v.GetString("worker.exchange.input"),
+			OutputExchange: v.GetString("worker.exchange.output"),
+			MessageBroker:  v.GetString("worker.broker"),
 		},
 	})
 
 	defer filter.CloseWorker()
 
-	err := filter.RunWorker()
+	err = filter.RunWorker()
 	if err != nil {
 		panic(err)
 	}
