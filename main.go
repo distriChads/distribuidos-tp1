46bc498ef16dc171a49ac8987bb7455575700132
belfortep <pbelforte2003@gmail.com>
2025-04-18 18:10:11 -0300

    test filter after a filter works

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"distribuidos-tp1/common/worker"
 	"distribuidos-tp1/filters/filter_after_2000"
+	"distribuidos-tp1/filters/filter_argentina"
 	"fmt"
 	"time"
 
@@ -26,13 +27,13 @@ func main() {
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"input_exchange", // name
-		"fanout",         // type
-		true,             // durable
-		false,            // auto-deleted
-		false,            // internal
-		false,            // no-wait
-		nil,              // arguments
+		"argentina_input_exchange", // name
+		"fanout",                   // type
+		true,                       // durable
+		false,                      // auto-deleted
+		false,                      // internal
+		false,                      // no-wait
+		nil,                        // arguments
 	)
 	if err != nil {
 		println(err.Error())
@@ -40,55 +41,51 @@ func main() {
 	}
 
 	err = ch.ExchangeDeclare(
-		"output_exchange", // name
-		"fanout",          // type
-		true,              // durable
-		false,             // auto-deleted
-		false,             // internal
-		false,             // no-wait
-		nil,               // arguments
+		"argentina_output_exchange", // name
+		"fanout",                    // type
+		true,                        // durable
+		false,                       // auto-deleted
+		false,                       // internal
+		false,                       // no-wait
+		nil,                         // arguments
 	)
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
+	err = ch.ExchangeDeclare(
+		"after_2000_output_exchange", // name
+		"fanout",                     // type
+		true,                         // durable
+		false,                        // auto-deleted
+		false,                        // internal
+		false,                        // no-wait
+		nil,                          // arguments
+	)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	filterArgentina := filter_argentina.NewFilterByArgentina(filter_argentina.FilterByArgentinaConfig{
+		WorkerConfig: worker.WorkerConfig{
+			InputExchange:  "argentina_input_exchange",
+			OutputExchange: "argentina_output_exchange",
+			MessageBroker:  "amqp://guest:guest@localhost:5672/",
+		},
+	})
+
 	filterAfterYear2000 := filter_after_2000.NewFilterByAfterYear2000(filter_after_2000.FilterByAfterYear2000Config{
 		WorkerConfig: worker.WorkerConfig{
-			InputExchange:  "input_exchange",
-			OutputExchange: "output_exchange",
+			InputExchange:  "argentina_output_exchange",
+			OutputExchange: "after_2000_output_exchange",
 			MessageBroker:  "amqp://guest:guest@localhost:5672/",
 		},
 	})
-
-	/*filterArgentina := filters.NewFilterByArgentina(filters.FilterByArgentinaConfig{
-		WorkerConfig: worker.WorkerConfig{
-			InputExchange:  "input_exchange",
-			OutputExchange: "output_exchange",
-			MessageBroker:  "amqp://guest:guest@localhost:5672/",
-		},
-	})
-
-	filterSpainAndOf2000 := filters.NewFilterBySpainAndOf2000(filters.FilterBySpainAndOf2000Config{
-		WorkerConfig: worker.WorkerConfig{
-			InputExchange:  "input_exchange",
-			OutputExchange: "output_exchange",
-			MessageBroker:  "amqp://guest:guest@localhost:5672/",
-		},
-	})
-
-	filterOnlyOneCountry := filters.NewFilterByOnlyOneCountry(filters.FilterByOnlyOneCountryConfig{
-		WorkerConfig: worker.WorkerConfig{
-			InputExchange:  "input_exchange",
-			OutputExchange: "output_exchange",
-			MessageBroker:  "amqp://guest:guest@localhost:5672/",
-		},
-	})*/
 
 	defer filterAfterYear2000.CloseWorker()
-	//defer filterArgentina.CloseWorker()
-	//defer filterSpainAndOf2000.CloseWorker()
-	//defer filterOnlyOneCountry.CloseWorker()
+	defer filterArgentina.CloseWorker()
 
 	// Set up a consumer for the output exchange
 	outputQueue, err := ch.QueueDeclare(
@@ -105,11 +102,11 @@ func main() {
 	}
 
 	err = ch.QueueBind(
-		outputQueue.Name,  // queue name
-		"",                // routing key
-		"output_exchange", // exchange
-		false,             // no-wait
-		nil,               // arguments
+		outputQueue.Name,             // queue name
+		"",                           // routing key
+		"after_2000_output_exchange", // exchange
+		false,                        // no-wait
+		nil,                          // arguments
 	)
 	if err != nil {
 		println("Failed to bind output queue:", err.Error())
@@ -130,14 +127,10 @@ func main() {
 		return
 	}
 
+	go filterArgentina.RunWorker()
+	time.Sleep(2 * time.Second)
 	go filterAfterYear2000.RunWorker()
 	time.Sleep(2 * time.Second)
-	//go filterArgentina.RunWorker()
-	//time.Sleep(2 * time.Second)
-	//go filterSpainAndOf2000.RunWorker()
-	//time.Sleep(2 * time.Second)
-	//go filterOnlyOneCountry.RunWorker()
-	//time.Sleep(2 * time.Second)
 
 	// Produce 3 messages to input_exchange
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -146,19 +139,19 @@ func main() {
 	// Send 3 sample messages to the input exchange
 
 	line := `
-	USA|ARG|CHI,2002-10-30,Comedy|Family|Action,toy story,id1
+	USA|ARG|CHI,2006-10-30,Comedy|Family|Action,toy story,id1
 	CAN|ARG|BRA,1996-10-30,Comedy|Family|Action,megamente,id2
-	ARG|CHI|SPAIN,2009-10-30,Comedy|Family|Action,shrek,id3
-	USA,1992-10-30,Comedy|Family|Action,cars,id4
+	ARG|CHI|SPAIN,2004-10-30,Comedy|Family|Action,shrek,id3
+	USA,2008-10-30,Comedy|Family|Action,cars,id4
 	`
 
 	for i := 1; i <= 3; i++ {
 		message := fmt.Sprintf("%v", line)
 		err = ch.PublishWithContext(ctx,
-			"input_exchange", // exchange
-			"",               // routing key
-			false,            // mandatory
-			false,            // immediate
+			"argentina_input_exchange", // exchange
+			"",                         // routing key
+			false,                      // mandatory
+			false,                      // immediate
 			amqp.Publishing{
 				ContentType: "text/plain",
 				Body:        []byte(message),
@@ -186,7 +179,8 @@ func main() {
 		}
 	}
 
-	ch.ExchangeDelete("input_exchange", false, false)
-	ch.ExchangeDelete("output_exchange", false, false)
+	ch.ExchangeDelete("argentina_input_exchange", false, false)
+	ch.ExchangeDelete("argentina_output_exchange", false, false)
+	ch.ExchangeDelete("after_2000_output_exchange", false, false)
 	println("Exchanges deleted")
 }
