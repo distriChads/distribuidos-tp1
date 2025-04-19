38aacb4562b6a66393e9ace7ba93bff40306928a
belfortep <pbelforte2003@gmail.com>
2025-04-18 23:01:22 -0300

    group by country and sum

---
+++ group_by/group_by_country_sum/group_by.go
@@ -0,0 +1,106 @@
+package group_by_country_sum
+
+import (
+	worker "distribuidos-tp1/common/worker"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/op/go-logging"
+)
+
+type GroupByCountryAndSumConfig struct {
+	worker.WorkerConfig
+}
+
+type GroupByCountryAndSum struct {
+	worker.Worker
+	messages_before_commit int
+}
+
+var log = logging.MustGetLogger("filter_by_year")
+
+func groupByCountryAndSum(lines []string, grouped_elements map[string]int) {
+	for _, line := range lines {
+		parts := strings.Split(line, ",")
+		budget, err := strconv.Atoi(parts[1])
+		if err != nil {
+			continue
+		}
+		grouped_elements[parts[0]] += budget
+	}
+}
+
+func storeGroupedElements(results map[string]int) {
+	// TODO: Dumpear el hashmap a un archivo
+}
+
+func mapToLines(grouped_elements map[string]int) []string {
+	var lines []string
+	for country, budget := range grouped_elements {
+		line := fmt.Sprintf("%s,%d", country, budget)
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func getGroupedElements() map[string]int {
+	// TODO: Cuando se caiga un worker, deberia leer de este archivo lo que estuvo obteniendo
+	return nil
+}
+
+func NewGroupByCountryAndSum(config GroupByCountryAndSumConfig, messages_before_commit int) *GroupByCountryAndSum {
+	log.Infof("GroupByCountryAndSum: %+v", config)
+	return &GroupByCountryAndSum{
+		Worker: worker.Worker{
+			InputExchange:  config.InputExchange,
+			OutputExchange: config.OutputExchange,
+			MessageBroker:  config.MessageBroker,
+		},
+		messages_before_commit: messages_before_commit,
+	}
+}
+
+func (f *GroupByCountryAndSum) RunWorker() error {
+	log.Info("Starting GroupByCountryAndSum worker")
+	worker.InitSender(&f.Worker)
+	worker.InitReceiver(&f.Worker)
+
+	msgs, err := worker.ReceivedMessages(f.Worker)
+	if err != nil {
+		log.Errorf("Error initializing receiver: %s", err.Error())
+		return err
+	}
+	messages_before_commit := 0
+	grouped_elements := make(map[string]int)
+	for message := range msgs {
+		log.Infof("Received message in group by country and sum: %s", string(message.Body))
+		message := string(message.Body)
+		if message == "EOF" {
+			break
+		}
+		messages_before_commit += 1
+		lines := strings.Split(strings.TrimSpace(message), "\n")
+		groupByCountryAndSum(lines, grouped_elements)
+		if messages_before_commit >= f.messages_before_commit {
+			storeGroupedElements(grouped_elements)
+			messages_before_commit = 0
+		}
+	}
+	message_to_send := mapToLines(grouped_elements)
+	err = worker.SendMessage(f.Worker, []byte(strings.Join(message_to_send, "\n")))
+	// TODO: Enviar a una cola de un agrupador "maestro" que haga la ultima agrupacion y este se lo envie al proximo chavoncito
+	if err != nil {
+		log.Infof("Error sending message: %s", err.Error())
+	}
+	return nil
+}
+
+func (f *GroupByCountryAndSum) CloseWorker() error {
+	err := worker.CloseSender(&f.Worker)
+	if err != nil {
+		return err
+	}
+
+	return worker.CloseReceiver(&f.Worker)
+}
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"distribuidos-tp1/common/worker"
-	"distribuidos-tp1/filters/filter_after_2000"
-	"distribuidos-tp1/filters/filter_argentina"
+	"distribuidos-tp1/filters/filter_only_one_country"
+	"distribuidos-tp1/group_by/group_by_country_sum"
 	"fmt"
 	"time"
 
@@ -27,13 +27,13 @@ func main() {
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"argentina_input_exchange", // name
-		"fanout",                   // type
-		true,                       // durable
-		false,                      // auto-deleted
-		false,                      // internal
-		false,                      // no-wait
-		nil,                        // arguments
+		"result_group_by_output_exchange", // name
+		"fanout",                          // type
+		true,                              // durable
+		false,                             // auto-deleted
+		false,                             // internal
+		false,                             // no-wait
+		nil,                               // arguments
 	)
 	if err != nil {
 		println(err.Error())
@@ -41,13 +41,13 @@ func main() {
 	}
 
 	err = ch.ExchangeDeclare(
-		"argentina_output_exchange", // name
-		"fanout",                    // type
-		true,                        // durable
-		false,                       // auto-deleted
-		false,                       // internal
-		false,                       // no-wait
-		nil,                         // arguments
+		"filter_only_one_output_exchange", // name
+		"fanout",                          // type
+		true,                              // durable
+		false,                             // auto-deleted
+		false,                             // internal
+		false,                             // no-wait
+		nil,                               // arguments
 	)
 	if err != nil {
 		println(err.Error())
@@ -55,20 +55,20 @@ func main() {
 	}
 
 	err = ch.ExchangeDeclare(
-		"after_2000_output_exchange", // name
-		"fanout",                     // type
-		true,                         // durable
-		false,                        // auto-deleted
-		false,                        // internal
-		false,                        // no-wait
-		nil,                          // arguments
+		"filter_only_one_input_exchange", // name
+		"fanout",                         // type
+		true,                             // durable
+		false,                            // auto-deleted
+		false,                            // internal
+		false,                            // no-wait
+		nil,                              // arguments
 	)
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
-	filterArgentina := filter_argentina.NewFilterByArgentina(filter_argentina.FilterByArgentinaConfig{
+	/*filterArgentina := filter_argentina.NewFilterByArgentina(filter_argentina.FilterByArgentinaConfig{
 		WorkerConfig: worker.WorkerConfig{
 			InputExchange:  "argentina_input_exchange",
 			OutputExchange: "argentina_output_exchange",
@@ -82,10 +82,26 @@ func main() {
 			OutputExchange: "after_2000_output_exchange",
 			MessageBroker:  "amqp://guest:guest@localhost:5672/",
 		},
+	})*/
+
+	filterOnlyOneCountry := filter_only_one_country.NewFilterByOnlyOneCountry(filter_only_one_country.FilterByOnlyOneCountryConfig{
+		WorkerConfig: worker.WorkerConfig{
+			InputExchange:  "filter_only_one_input_exchange",
+			OutputExchange: "filter_only_one_output_exchange",
+			MessageBroker:  "amqp://guest:guest@localhost:5672/",
+		},
 	})
 
-	defer filterAfterYear2000.CloseWorker()
-	defer filterArgentina.CloseWorker()
+	groupByCountryAndSum := group_by_country_sum.NewGroupByCountryAndSum(group_by_country_sum.GroupByCountryAndSumConfig{
+		WorkerConfig: worker.WorkerConfig{
+			InputExchange:  "filter_only_one_output_exchange",
+			OutputExchange: "result_group_by_output_exchange",
+			MessageBroker:  "amqp://guest:guest@localhost:5672/",
+		},
+	}, 10)
+
+	defer filterOnlyOneCountry.CloseWorker()
+	defer groupByCountryAndSum.CloseWorker()
 
 	// Set up a consumer for the output exchange
 	outputQueue, err := ch.QueueDeclare(
@@ -102,11 +118,11 @@ func main() {
 	}
 
 	err = ch.QueueBind(
-		outputQueue.Name,             // queue name
-		"",                           // routing key
-		"after_2000_output_exchange", // exchange
-		false,                        // no-wait
-		nil,                          // arguments
+		outputQueue.Name,                  // queue name
+		"",                                // routing key
+		"result_group_by_output_exchange", // exchange
+		false,                             // no-wait
+		nil,                               // arguments
 	)
 	if err != nil {
 		println("Failed to bind output queue:", err.Error())
@@ -127,9 +143,9 @@ func main() {
 		return
 	}
 
-	go filterArgentina.RunWorker()
+	go filterOnlyOneCountry.RunWorker()
 	time.Sleep(2 * time.Second)
-	go filterAfterYear2000.RunWorker()
+	go groupByCountryAndSum.RunWorker()
 	time.Sleep(2 * time.Second)
 
 	// Produce 3 messages to input_exchange
@@ -138,20 +154,32 @@ func main() {
 
 	// Send 3 sample messages to the input exchange
 
-	line := `
+	/*line = `
 	USA|ARG|CHI,2006-10-30,Comedy|Family|Action,toy story,id1
 	CAN|ARG|BRA,1996-10-30,Comedy|Family|Action,megamente,id2
 	ARG|CHI|SPAIN,2004-10-30,Comedy|Family|Action,shrek,id3
 	USA,2008-10-30,Comedy|Family|Action,cars,id4
+	`*/
+	line := `
+	USA|ARG|CHI,1500
+	CAN|ARG|BRA,2000
+	ARG|CHI|SPAIN,3000
+	USA,1000
+	ARG,4000
+	CHI,6000
+	USA,4000
+	ARG,500
+	BRA|CHI,700
+	BRA,800
 	`
 
 	for i := 1; i <= 3; i++ {
 		message := fmt.Sprintf("%v", line)
 		err = ch.PublishWithContext(ctx,
-			"argentina_input_exchange", // exchange
-			"",                         // routing key
-			false,                      // mandatory
-			false,                      // immediate
+			"filter_only_one_input_exchange", // exchange
+			"",                               // routing key
+			false,                            // mandatory
+			false,                            // immediate
 			amqp.Publishing{
 				ContentType: "text/plain",
 				Body:        []byte(message),
@@ -159,6 +187,22 @@ func main() {
 		if err != nil {
 			println("Failed to publish message:", err.Error())
 			return
+		}
+		if i == 3 {
+			message = "EOF"
+			err = ch.PublishWithContext(ctx,
+				"filter_only_one_input_exchange", // exchange
+				"",                               // routing key
+				false,                            // mandatory
+				false,                            // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(message),
+				})
+			if err != nil {
+				println("Failed to publish message:", err.Error())
+				return
+			}
 		}
 		println("Published message:", message)
 		time.Sleep(500 * time.Millisecond) // Small delay between messages
@@ -179,8 +223,8 @@ func main() {
 		}
 	}
 
-	ch.ExchangeDelete("argentina_input_exchange", false, false)
-	ch.ExchangeDelete("argentina_output_exchange", false, false)
-	ch.ExchangeDelete("after_2000_output_exchange", false, false)
+	ch.ExchangeDelete("filter_only_one_input_exchange", false, false)
+	ch.ExchangeDelete("filter_only_one_output_exchange", false, false)
+	ch.ExchangeDelete("result_group_by_output_exchange", false, false)
 	println("Exchanges deleted")
 }
