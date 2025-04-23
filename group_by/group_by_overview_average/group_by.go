c4d8f6e7bbd99f44cf9bfeabb3a2662734d838b8
belfortep <pbelforte2003@gmail.com>
2025-04-23 19:01:56 -0300

    ready group by overview, added to docker compose

---
+++ group_by/group_by_overview_average/group_by.go
@@ -0,0 +1,125 @@
+package group_by_movie_avg
+
+import (
+	worker "distribuidos-tp1/common/worker/worker"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/op/go-logging"
+)
+
+type GroupByOverviewAndAvgConfig struct {
+	worker.WorkerConfig
+}
+
+var log = logging.MustGetLogger("group_by_movie_average")
+
+type GroupByOverviewAndAvg struct {
+	worker.Worker
+	messages_before_commit int
+}
+
+type RevenueBudgetCount struct {
+	count   int
+	revenue float64
+	budget  float64
+}
+
+// ---------------------------------
+// MESSAGE FORMAT: OVERVIEW|BUDGET|REVENUE
+// ---------------------------------
+const OVERVIEW = 0
+const BUDGET = 1
+const REVENUE = 2
+
+func groupByOverviewAndUpdate(lines []string, grouped_elements map[string]RevenueBudgetCount) {
+	for _, line := range lines {
+		parts := strings.Split(line, worker.MESSAGE_SEPARATOR)
+		budget, err := strconv.ParseFloat(parts[BUDGET], 64)
+		if err != nil {
+			continue
+		}
+		revenue, err := strconv.ParseFloat(parts[REVENUE], 64)
+		if err != nil {
+			continue
+		}
+
+		current := grouped_elements[parts[OVERVIEW]]
+		current.budget += budget
+		current.revenue += revenue
+		current.count += 1
+		grouped_elements[parts[OVERVIEW]] = current
+
+	}
+}
+
+func storeGroupedElements(results map[string]RevenueBudgetCount) {
+	// TODO: Dumpear el hashmap a un archivo
+}
+
+func mapToLines(grouped_elements map[string]RevenueBudgetCount) string {
+	var lines []string
+	for overview, value := range grouped_elements {
+		result := value.revenue / value.budget
+		average := result / float64(value.count)
+		line := fmt.Sprintf("%s%s%f", overview, worker.MESSAGE_SEPARATOR, average)
+		lines = append(lines, line)
+	}
+	return strings.Join(lines, "\n")
+}
+
+func getGroupedElements() map[string]int {
+	// TODO: Cuando se caiga un worker, deberia leer de este archivo lo que estuvo obteniendo
+	return nil
+}
+
+func NewGroupByOverviewAndAvg(config GroupByOverviewAndAvgConfig, messages_before_commit int) *GroupByOverviewAndAvg {
+	log.Infof("GroupByOverviewAndAvg: %+v", config)
+	return &GroupByOverviewAndAvg{
+		Worker: worker.Worker{
+			InputExchange:  config.InputExchange,
+			OutputExchange: config.OutputExchange,
+			MessageBroker:  config.MessageBroker,
+		},
+		messages_before_commit: messages_before_commit,
+	}
+}
+
+func (f *GroupByOverviewAndAvg) RunWorker() error {
+	log.Info("Starting GroupByOverviewAndAvg worker")
+	worker.InitSender(&f.Worker)
+	worker.InitReceiver(&f.Worker)
+
+	msgs, err := worker.ReceivedMessages(f.Worker)
+	if err != nil {
+		log.Errorf("Error initializing receiver: %s", err.Error())
+		return err
+	}
+	messages_before_commit := 0
+	grouped_elements := make(map[string]RevenueBudgetCount)
+	for message := range msgs {
+		message := string(message.Body)
+		if message == worker.MESSAGE_EOF {
+			break
+		}
+		messages_before_commit += 1
+		lines := strings.Split(strings.TrimSpace(message), "\n")
+		groupByOverviewAndUpdate(lines, grouped_elements)
+		if messages_before_commit >= f.messages_before_commit {
+			storeGroupedElements(grouped_elements)
+			messages_before_commit = 0
+		}
+	}
+	message_to_send := mapToLines(grouped_elements)
+	err = worker.SendMessage(f.Worker, message_to_send)
+	// TODO: Enviar a una cola de un agrupador "maestro" que haga la ultima agrupacion y este se lo envie al proximo chavoncito
+	if err != nil {
+		log.Infof("Error sending message: %s", err.Error())
+	}
+	err = worker.SendMessage(f.Worker, worker.MESSAGE_EOF)
+	if err != nil {
+		log.Infof("Error sending message: %s", err.Error())
+	}
+	return nil
+}
---
+++ group_by/group_by_overview_average/main/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"distribuidos-tp1/common/utils"
+	"distribuidos-tp1/common/worker/worker"
+
+	group_by "distribuidos-tp1/group_by/group_by_movie_average"
+
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("group_by_movie_average")
+
+func main() {
+	v, err := utils.InitConfig()
+	if err != nil {
+		log.Criticalf("%s", err)
+		return
+	}
+
+	log_level := v.GetString("log.level")
+	inputExchangeSpec := worker.ExchangeSpec{
+		Name:        v.GetString("worker.exchange.input.name"),
+		RoutingKeys: []string{v.GetString("worker.exchange.input.routingKeys")},
+		QueueName:   "group_by_movie_average_queue",
+	}
+	outputExchangeSpec := worker.ExchangeSpec{
+		Name:        v.GetString("worker.exchange.output.name"),
+		RoutingKeys: []string{v.GetString("worker.exchange.output.routingKeys")},
+		QueueName:   "group_by_movie_average_queue",
+	}
+	messageBroker := v.GetString("worker.broker")
+
+	if inputExchangeSpec.Name == "" || inputExchangeSpec.RoutingKeys[0] == "" || outputExchangeSpec.Name == "" || outputExchangeSpec.RoutingKeys[0] == "" || messageBroker == "" {
+		log.Criticalf("Error: one or more environment variables are empty")
+		return
+	}
+
+	if err := utils.InitLogger(log_level); err != nil {
+		log.Criticalf("%s", err)
+		return
+	}
+
+	maxMessages := v.GetInt("worker.maxmessages")
+	if maxMessages == 0 {
+		maxMessages = 10
+	}
+
+	groupByMovieAverage := group_by.NewGroupByMovieAndAvg(group_by.GroupByMovieAndAvgConfig{
+		WorkerConfig: worker.WorkerConfig{
+			InputExchange:  inputExchangeSpec,
+			OutputExchange: outputExchangeSpec,
+			MessageBroker:  messageBroker,
+		},
+	}, maxMessages)
+
+	defer groupByMovieAverage.CloseWorker()
+
+	err = groupByMovieAverage.RunWorker()
+	if err != nil {
+		panic(err)
+	}
+}
